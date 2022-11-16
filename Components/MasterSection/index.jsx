26b20720ea191419f2248ac1187484c9d48860f1
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2022-11-16 20:11:50 +0200

    updates

--- Components/MasterSection/index.jsx
+++ Components/MasterSection/index.jsx
@@ -6,7 +6,7 @@ import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 const MasterSection = () => {
   return (
     <section className={`${styles.section} section__spaces`}>
-      <div className="conatiner">
+      <div className="container">
         <div className={styles.section__content}>
           <blockquote  className={styles.ph}><span>"</span>Master your strength, outsource your weaknesses<span>"</span></blockquote>
           <span className={styles.auther}><HiChevronDoubleRight className={styles.icon} /> Ryan Khan <HiChevronDoubleLeft className={styles.icon} /></span>
