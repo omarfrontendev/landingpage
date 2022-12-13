88d68971d9f188669f6586cfc6337e33801b3534
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2022-12-13 22:14:56 +0200

    updates

--- Components/ContactUs/index.jsx
+++ Components/ContactUs/index.jsx
@@ -1,104 +1,123 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import emailjs from 'emailjs-com';
-import Input from '../Input'
-import styles from './.module.scss'
-import { motion, AnimatePresence } from 'framer-motion'
-import AppContext from '../../store/app-context'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import Input from "../Input";
+import styles from "./.module.scss";
+import { motion, AnimatePresence } from "framer-motion";
+import AppContext from "../../store/app-context";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactUs = () => {
-  
   const form = useRef();
   const ctx = useContext(AppContext);
   const contactSection = useRef(null);
   useEffect(() => {
-    ctx.setContactOffset(contactSection.current.offsetTop)
+    ctx.setContactOffset(contactSection.current.offsetTop);
   }, [ctx]);
   const [data, setData] = useState({
-    service: 'Admin/Virtual Assistance', 
-    message__body: '',
-    message__title: ''
+    service: "Admin/Virtual Assistance",
+    message__body: "",
+    message__title: "",
   });
-  const [inputBlur, setInputBlur] = useState(false)
+  const [inputBlur, setInputBlur] = useState(false);
   const enteredValueIsValid = data?.message__body?.length > 0;
   const errorInput = !enteredValueIsValid && inputBlur;
 
   const resources = [
     {
-      id: 'name',
-      placeholder: 'Name',
-      type: 'text',
-      checkTheValue: value => value?.length > 0,
+      id: "name",
+      placeholder: "Name",
+      type: "text",
+      checkTheValue: (value) => value?.length > 0,
       error: "This filed can't be empty!",
       required: true,
     },
     {
-      id: 'email',
-      placeholder: 'Email',
-      type: 'email',
-      checkTheValue: value => value?.includes('@'),
+      id: "email",
+      placeholder: "Email",
+      type: "email",
+      checkTheValue: (value) => value?.includes("@"),
       error: "Email must includes '@'!",
-      required: true
-    }
+      required: true,
+    },
   ];
   const services = [
     {
-      id: 'service',
-      placeholder: 'Service',
-      type: 'select__box',
+      id: "service",
+      placeholder: "Service",
+      type: "select__box",
       options: [
-        'Admin/Virtual Assistance',
-        'Social Media Moderation',
-        'LinkedIn Organic growth',
-        'Customer Service',
-        'Data Entry/Internet research',
-        'Graphic Design',
-        'Video Editing',
-        'Media Buying',
-        'Other'
+        "Admin/Virtual Assistance",
+        "Social Media Moderation",
+        "LinkedIn Organic growth",
+        "Customer Service",
+        "Data Entry/Internet research",
+        "Graphic Design",
+        "Video Editing",
+        "Media Buying",
+        "Other",
       ],
-      required: true
+      required: true,
     },
     {
-      id: 'other__services',
-      placeholder: 'Other',
-      type: 'text',
+      id: "other__services",
+      placeholder: "Other",
+      type: "text",
       required: false,
-      checkTheValue: value => value?.length > 0,
+      checkTheValue: (value) => value?.length > 0,
       error: "This filed can't be empty!",
     },
   ];
 
   let formValidation = false;
-  if(data?.name !== '' && data?.email?.includes('@') &&  data.message__body !== '' && data.message__title !== '') {
+  if (
+    data?.name !== "" &&
+    data?.email?.includes("@") &&
+    data.message__body !== "" &&
+    data.message__title !== ""
+  ) {
     formValidation = true;
-  };
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_h63f6wv', 'template_wai1k3i', form.current, 'nU4oy23aRmqubMFMX')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_n952gc8",
+        "template_pnpg606",
+        form.current,
+        "FkTRV49FMJUkGpazk"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      toast.success(`Thank you ${data.name}, we have received your message and we will get back to you as soon as possible`, {
-        position: toast.POSITION.TOP_RIGHT
-      });
-      e.target.reset();
-      setData({
-        service: 'Admin/Virtual Assistance', 
-        message__body: '',
-        message__title: ''
-      });
+        }
+      );
+    toast.success(`received`, {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+    e.target.reset();
+    setData({
+      service: "Admin/Virtual Assistance",
+      message__body: "",
+      message__title: "",
+    });
   };
 
   return (
-    <section id='contact__us' ref={contactSection} className='section__spaces container'>
-      <h2 className='section__title'><span>Contact Us</span></h2>
+    <section
+      id="contact__us"
+      ref={contactSection}
+      className="section__spaces container"
+    >
+      <h2 className="section__title">
+        <span>Contact Us</span>
+      </h2>
       <div className={`${styles.contact__us__content} d-flex`}>
         <div className={styles.left__col}>
           <div className={styles.image__container}>
@@ -107,10 +126,10 @@ const ContactUs = () => {
         </div>
         <div className={styles.contact__form__container}>
           <form ref={form} onSubmit={sendEmail} className={styles.form}>
-          <div className={styles.label}>Your Info:</div>
+            <div className={styles.label}>Your Info:</div>
             <div className={styles.inputs__group}>
-              {resources.map(input => (
-                <Input 
+              {resources.map((input) => (
+                <Input
                   key={input.id}
                   id={input.id}
                   placeholder={input.placeholder}
@@ -125,7 +144,7 @@ const ContactUs = () => {
               ))}
             </div>
             <motion.div className={styles.services}>
-              <Input 
+              <Input
                 id={services[0].id}
                 placeholder={services[0].placeholder}
                 required={services[0].required}
@@ -135,23 +154,25 @@ const ContactUs = () => {
                 options={services[0].options}
               />
               <AnimatePresence>
-                {data.service === 'Other' && (
+                {data.service === "Other" && (
                   <div className={styles.select__box__container}>
                     <motion.input
-                      animate={{height: '50px'}}
-                      exit={{height: '0px'}} 
-                      initial={{height: '0px'}}
-                      transition={{type: 'linear'}}
+                      animate={{ height: "50px" }}
+                      exit={{ height: "0px" }}
+                      initial={{ height: "0px" }}
+                      transition={{ type: "linear" }}
                       id={services[1].id}
                       name={services[1].id}
                       placeholder={services[1].placeholder}
                       required={services[1].required}
                       type={services[1].type}
-                      value={data.other || ''}
-                      onChange={(e) => setData({
-                        ...data,
-                        other: e.target.value
-                      })}
+                      value={data.other || ""}
+                      onChange={(e) =>
+                        setData({
+                          ...data,
+                          other: e.target.value,
+                        })
+                      }
                       // ========
                       className={styles.input}
                     />
@@ -161,40 +182,51 @@ const ContactUs = () => {
             </motion.div>
             <div className={styles.label}>Your Message:</div>
             <div className={styles.message}>
-              <Input 
-                id='message__title'
-                placeholder='Message Title'
+              <Input
+                id="message__title"
+                placeholder="Message Title"
                 required={true}
-                type='text'
+                type="text"
                 setData={setData}
                 data={data}
-                checkTheValue={value => value?.length > 0}
+                checkTheValue={(value) => value?.length > 0}
                 error={"This filed can't be empty!"}
               />
               <div className={styles.select__box__container}>
-                <textarea 
-                  name="message__body" 
+                <textarea
+                  name="message__body"
                   className={styles.textarea}
-                  id="message__body" 
-                  placeholder='Message Body'
-                  onChange={(e) => setData({
-                    ...data,
-                    message__body: e.target.value
-                  })}
+                  id="message__body"
+                  placeholder="Message Body"
+                  onChange={(e) =>
+                    setData({
+                      ...data,
+                      message__body: e.target.value,
+                    })
+                  }
                   onBlur={() => {
-                    setInputBlur(true)
+                    setInputBlur(true);
                   }}
-
                 />
-                {errorInput && <p className={styles.error__message}>{"This filed can't be empty!"}</p>}
+                {errorInput && (
+                  <p className={styles.error__message}>
+                    {"This filed can't be empty!"}
+                  </p>
+                )}
               </div>
             </div>
-            <button type='submit' disabled={!formValidation} className={styles.submit__btn}>Send</button>
+            <button
+              type="submit"
+              disabled={!formValidation}
+              className={styles.submit__btn}
+            >
+              Send
+            </button>
           </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
