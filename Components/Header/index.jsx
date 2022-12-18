3850d8016dcd9ea265e13169eb09d56f6e841b98
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2022-12-18 16:08:02 +0200

    Update the project

--- Components/Header/index.jsx
+++ Components/Header/index.jsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
-import Image from 'next/image'
-import Link from 'next/link'
-import Logo from "../../public/assets/logo.png"
-import styles from './.module.scss'
-import Aos from 'aos';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Logo from "../../public/assets/logo.png";
+import styles from "./.module.scss";
+import Aos from "aos";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import "aos/dist/aos.css";
 
 const Header = () => {
-
   const [openMenu, setOpenMenu] = useState(false);
 
   useEffect(() => {
@@ -18,33 +16,59 @@ const Header = () => {
   return (
     <header className={`${styles.header} d-flex align-items-center`}>
       <div className="container">
-        <div className={`${styles.header__content} d-flex align-items-center justify-content-between`}>
-          <Link href={'/'} className={styles.logo__container}>
-            <Image src={Logo} alt="LOGO" className={styles.logo} />
+        <div
+          className={`${styles.header__content} d-flex align-items-center justify-content-between`}
+        >
+          <Link href={"/"} className={styles.logo__container}>
+            <img src={Logo} alt="LOGO" className={styles.logo} />
           </Link>
-          <ul className={`${styles.links__list} ${openMenu ? styles.opened : ''} d-flex align-items-center justify-content-end`}>
+          <ul
+            className={`${styles.links__list} ${
+              openMenu ? styles.opened : ""
+            } d-flex align-items-center justify-content-end`}
+          >
             <li onClick={() => setOpenMenu(false)}>
-              <a className={`${styles.link} ${styles.active}`} href="#home">Home</a>
+              <a className={`${styles.link} ${styles.active}`} href="#home">
+                Home
+              </a>
             </li>
             <li onClick={() => setOpenMenu(false)}>
-              <a className={styles.link} href="#services">Services</a>
+              <a className={styles.link} href="#services">
+                Services
+              </a>
             </li>
             <li onClick={() => setOpenMenu(false)}>
-              <a className={styles.link} href="#our__team">Our Team</a>
+              <a className={styles.link} href="#our__team">
+                Our Team
+              </a>
             </li>
             <li onClick={() => setOpenMenu(false)}>
-              <a className={styles.link} href="#about__us">About Us</a>
+              <a className={styles.link} href="#about__us">
+                About Us
+              </a>
             </li>
             <li onClick={() => setOpenMenu(false)}>
-              <a className={styles.link} href="#contact__us">Contact US</a>
+              <a className={styles.link} href="#contact__us">
+                Contact US
+              </a>
             </li>
-            <button className={`d-block d-md-none ${styles.close__btn}`} onClick={() => setOpenMenu(false)}><AiOutlineClose /></button>
+            <button
+              className={`d-block d-md-none ${styles.close__btn}`}
+              onClick={() => setOpenMenu(false)}
+            >
+              <AiOutlineClose />
+            </button>
           </ul>
-          <button className={`d-block d-md-none ${styles.open__btn}`} onClick={() => setOpenMenu(true)}><AiOutlineMenu /></button>
+          <button
+            className={`d-block d-md-none ${styles.open__btn}`}
+            onClick={() => setOpenMenu(true)}
+          >
+            <AiOutlineMenu />
+          </button>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
